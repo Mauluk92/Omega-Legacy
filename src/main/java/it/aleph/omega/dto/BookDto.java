e982d336ebf17afb8931cae1bfb933799a309a5d
Nicola <ncl.rss.92@gmail.com>
2024-01-15 11:57:30 +0100

    Added mapstruct and lombok as dependencies, added DTO layer

---
+++ src/main/java/it/aleph/omega/dto/BookDto.java
@@ -0,0 +1,26 @@
+package it.aleph.omega.dto;
+
+import it.aleph.omega.model.Author;
+import it.aleph.omega.model.Tag;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+@Getter
+@Setter
+public class BookDto {
+
+    private String title;
+    private String isbn;
+    private String deweyDecimalCode;
+    private String contentDescription;
+    private LocalDate pubDate;
+    private String pubHouse;
+    private Boolean available;
+    private BigDecimal price;
+    private List<Author> authorList;
+    private List<Tag> tagList;
+}
